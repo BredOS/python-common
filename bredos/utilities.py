9e300c353af8212850c2814d34eae58528cbd87e
Bill Sideris <bill88t@feline.gr>
2025-05-20 09:53:46 +0300

    Move some more helper functions over

--- bredos/utilities.py
+++ bredos/utilities.py
@@ -19,6 +19,7 @@
 from threading import Lock
 from functools import wraps
 from time import monotonic
+import subprocess
 
 
 def debounce(wait):
@@ -95,3 +96,13 @@ def get_ram_size(unit: str = "KB") -> int:
     except FileNotFoundError:
         return 0
     return 0
+
+
+def elevated_file_write(filepath: str, content: str):
+    proc = subprocess.run(
+        ["pkexec", "tee", filepath], input=content.encode(), check=True
+    )
+
+
+def wrap_lines(lines: list, width: int) -> list:
+    return [wrapped for line in lines for wrapped in textwrap.wrap(line, width)]
